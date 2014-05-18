6c67f75ee4db67b62598152afd176b6a152fe8f9
r4isstatic <r4isstatic@outlook.com>
2014-05-18 12:22:14 +0100

    remove db links
    removing db links in order to populate things first

--- app/models.py
+++ app/models.py
@@ -4,7 +4,7 @@ class Blogger(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(200), index = True, unique = False)
 	url = db.Column(db.String(200), index = True, unique = False)
-	choices = db.relationship('Choice', backref = 'choice', lazy = 'dynamic')
+#	choices = db.relationship('Choice', backref = 'choice', lazy = 'dynamic')
 
 	def __repr__(self):
 		return 'Blogger name %r>' % (self.name)
@@ -14,7 +14,7 @@ class Question(db.Model):
 	title = db.Column(db.String(300), index = True, unique = False)
 	day = db.Column(db.Integer, index = True, unique = False)
 	category_id = db.Column(db.Integer, db.ForeignKey('category.id'))
-	choices = db.relationship('Choice', backref = 'choice', lazy = 'dynamic')
+#	choices = db.relationship('Choice', backref = 'choice', lazy = 'dynamic')
 
 	def __repr__(self):
 		return 'Question title %r>' % (self.title)
@@ -22,7 +22,7 @@ class Question(db.Model):
 class Category(db.Model):
 	id = db.Column(db.Integer, primary_key = True)
 	name = db.Column(db.String(200), index = True, unique = False)
-	questions = db.relationship('Question', backref = 'question', lazy = 'dynamic')
+#	questions = db.relationship('Question', backref = 'question', lazy = 'dynamic')
 
 	def __repr__(self):
 		return 'Category name %r>' % (self.name)
@@ -32,8 +32,8 @@ class Choice(db.Model):
 	url = db.Column(db.String(300), index = True, unique = False)
 	title = db.Column(db.String(300), index = True, unique = False) #eventually pull this out into a separate class of 'Song' and so on
 	year = db.Column(db.String(4), index = True, unique = False)
-	blogger_id = db.Column(db.Integer, db.ForeignKey('blogger.id'))
-	question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
+#	blogger_id = db.Column(db.Integer, db.ForeignKey('blogger.id'))
+#	question_id = db.Column(db.Integer, db.ForeignKey('question.id'))
 
 	def __repr__(self):
 		return 'Choice title %r>' % (self.title)
